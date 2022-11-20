648453aae8a6c586049edd409caf6d29056ef018
Farzan Mirshekari <fmirshek@uwaterloo.ca>
2022-11-20 18:32:10 -0500

    Client utilities..

--- prezzo-client/src/App.tsx
+++ prezzo-client/src/App.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <h2>Prezzo..</h2>
-  );
+    return <h2>Prezzo..</h2>
 }
 
-export default App;
+export default App
