f257a1dbab959c84324052f07d4cc9ae9d91d866
Farzan Mirshekari <fmirshek@uwaterloo.ca>
2022-11-22 21:40:19 -0500

    More server stuff..

--- prezzo-server/main.go
+++ prezzo-server/main.go
@@ -6,18 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type raw_content struct {
+	Text_Content string `json:"text_content"`
+}
+
 type slide struct {
 	Index   int                `json:"index"`
 	Content slide_text_content `json:"content"`
 }
 
 type slide_text_content struct {
-	Text_Content []string `json:"text_content"`
+	Text_Content string `json:"text_content"`
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/", deliver_message)
+	router.POST("/presentation_content", parse_presentation_content)
 	router.Run("localhost:8080")
 }
 
@@ -25,4 +30,17 @@ func deliver_message(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Prezzo server..",
 	})
+}
+
+func parse_presentation_content(c *gin.Context) {
+	var presentation_content raw_content
+	c.BindJSON(&presentation_content)
+	c.JSON(http.StatusOK, gin.H{
+		"content": presentation_content,
+		"status":  "OK",
+	})
+}
+
+func split_into_slides(presentation_content *raw_content) {
+	// TODO
 }
