2e9ea4bb0e670ee5e0f170a9c94717f016a6254a
Farzan Mirshekari <fmirshek@uwaterloo.ca>
2022-12-10 21:00:16 -0500

    Minor styling

--- prezzo-client/src/components/micro-components/Button.tsx
+++ prezzo-client/src/components/micro-components/Button.tsx
@@ -8,7 +8,7 @@ interface Props {
 function Button({ button_text, button_function }: Props) {
     return (
         <button
-            className="relative w-1/3 h-full flex flex-row justify-center items-center text-xl button"
+            className="relative w-1/3 h-full flex flex-row justify-center items-center text-base button"
             onClick={button_function}
         >
             {button_text}
